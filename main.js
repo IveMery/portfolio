3bca24a84bfa45c083fc0e0ac5dbbe0eb288830d
IvetteMery <ivette.mery@gmail.com>
2020-09-17 12:29:31 -0300

    agrego menu desplegable

---
+++ main.js
@@ -0,0 +1,24 @@
+
+ const btn_menu_responsive = document.querySelector('.menu_hamburguesa')
+
+ const menu_desplegable =document.querySelector('.menu-desplegable')
+ const lista_menu_movil =document.querySelectorAll('.lista_nav_movil')
+
+ console.log(btn_menu_responsive)
+ console.log(lista_menu_movil)
+ 
+ 
+ btn_menu_responsive.onclick= () =>{  
+     
+    menu_desplegable.classList.toggle('hidden')
+    
+
+ }
+
+for (let link of lista_menu_movil) {
+    link.onclick = () => {
+        menu_desplegable.classList.toggle('hidden')
+    }
+}
+
+
