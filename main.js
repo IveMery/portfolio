4a81b47fc043f6aa1cc87f28d992c40b7d90a044
IvetteMery <ivette.mery@gmail.com>
2020-09-20 21:54:40 -0300

    agrego filtro a seccion proyectos

--- main.js
+++ main.js
@@ -1,25 +1,34 @@
 
- const btn_menu_responsive = document.querySelector('.menu_hamburguesa')
+const btn_menu_responsive = document.querySelector('.menu_hamburguesa')
+const menu_desplegable = document.querySelector('.menu-desplegable')
+const lista_menu_movil = document.querySelectorAll('.lista_nav_movil')
+const cards = document.querySelectorAll('.card-container')
+const botonFiltro = document.querySelectorAll('.skills-list-button')
+// console.log(btn_menu_responsive)
+// console.log(lista_menu_movil)
+btn_menu_responsive.onclick = () => {
 
- const menu_desplegable =document.querySelector('.menu-desplegable')
- const lista_menu_movil =document.querySelectorAll('.lista_nav_movil')
-
- console.log(btn_menu_responsive)
- console.log(lista_menu_movil)
- 
- 
- btn_menu_responsive.onclick= () =>{  
-     
     menu_desplegable.classList.toggle('hidden')
-   
-    
 
- }
 
+
+}
 for (let link of lista_menu_movil) {
     link.onclick = () => {
         menu_desplegable.classList.toggle('hidden')
     }
 }
-
-
+for (let boton of botonFiltro) {
+    boton.onclick = () => {
+        for (let card of cards) {
+            if (boton.dataset.tecnologia === card.dataset.tecnologia) {
+                card.classList.remove('ocultar')
+            } else if (boton.dataset.tecnologia === "todos") {
+                card.classList.remove('ocultar')
+            }
+            else {
+                card.classList.add('ocultar')
+            }
+        }
+    }
+}
