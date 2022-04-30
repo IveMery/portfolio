d6550152986bc13b20f27efd7abb4bdff3032c4e
IvetteMery <ivette.mery@gmail.com>
2022-04-30 14:41:22 -0400

    agregando proyecto

--- main.js
+++ main.js
@@ -1,28 +1,27 @@
-const btn_menu_responsive = document.querySelector('.menu_hamburguesa')
-const menu_desplegable = document.querySelector('.menu-desplegable')
-const lista_menu_movil = document.querySelectorAll('.lista_nav_movil')
-const cards = document.querySelectorAll('.card-container')
-const botonFiltro = document.querySelectorAll('.skills-list-button')
+const btn_menu_responsive = document.querySelector(".menu_hamburguesa");
+const menu_desplegable = document.querySelector(".menu-desplegable");
+const lista_menu_movil = document.querySelectorAll(".lista_nav_movil");
+const cards = document.querySelectorAll(".card-container");
+const botonFiltro = document.querySelectorAll(".skills-list-button");
 
 btn_menu_responsive.onclick = () => {
-    menu_desplegable.classList.toggle('hidden')
-}
+  menu_desplegable.classList.toggle("hidden");
+};
 for (let link of lista_menu_movil) {
-    link.onclick = () => {
-        menu_desplegable.classList.toggle('hidden')
-    }
+  link.onclick = () => {
+    menu_desplegable.classList.toggle("hidden");
+  };
 }
 for (let boton of botonFiltro) {
-    boton.onclick = () => {
-        for (let card of cards) {
-            if (boton.dataset.tecnologia === card.dataset.tecnologia) {
-                card.classList.remove('ocultar')
-            } else if (boton.dataset.tecnologia === "todos") {
-                card.classList.remove('ocultar')
-            }
-            else {
-                card.classList.add('ocultar')
-            }
-        }
+  boton.onclick = () => {
+    for (let card of cards) {
+      if (boton.dataset.tecnologia === card.dataset.tecnologia) {
+        card.classList.remove("ocultar");
+      } else if (boton.dataset.tecnologia === "todos") {
+        card.classList.remove("ocultar");
+      } else {
+        card.classList.add("ocultar");
+      }
     }
+  };
 }
